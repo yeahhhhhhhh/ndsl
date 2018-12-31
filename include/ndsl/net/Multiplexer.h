d6fe9fa9d172a1e7b5d97da0cd17cc16f6875908
zzt <429834658@qq.com>
2018-12-31 17:36:37 +0800

    添加pthis指针

--- include/ndsl/net/Multiplexer.h
+++ include/ndsl/net/Multiplexer.h
@@ -1,6 +1,6 @@
-/** 
+/**
 * @file Multiplexer.h
-* @brief 
+* @brief
 *
 * 消息分发器，实现不同通信实体对tcp长连接的复用，每个通信实体拥有一个id，
 * 将通信实体的id与回调函数作为键值对存入map方便进行检索，回调相应的回调函数。
@@ -13,7 +13,7 @@
 *  5.分发消息给实体
 *
 * @author zzt
-* @emial 429834658@qq.com 
+* @emial 429834658@qq.com
 **/
 
 #ifndef __NDSL_NET_MULTIPLEXER_H__
@@ -21,12 +21,13 @@
 
 #include <map>
 #include <iostream>
-#include "ndsl/net/Connection.h" // connection抽象类
-#include "ndsl/net/Eventloop.h"
+//#include "ndsl/net/Connection.h" // connection抽象类
+#include "ndsl/net/TcpConnection.h"
+//#include "ndsl/net/EventLoop.h"
 namespace ndsl{
 namespace net{
 // 自定义消息结构体 TODO 小序
-#pragma pack(push) 
+#pragma pack(push)
 #pragma pack(1) // 一字节对齐
 struct Message{
     int id; // 通信实体的id
@@ -36,6 +37,7 @@ struct Message{
 
 // 自定义addwork传入的参数结构体
 struct para{
+    Multiplexer *pthis;
     int id;
     ndsl::net::Multiplexer::Callback cb;
 }
@@ -55,7 +57,7 @@ class Multiplexer
     // 回调函数映射容器
     using CallbackMap = std::map<int, Callback>;
 
-    ndsl::net::Connection *conn_; // 绑定的connection
+    ndsl::net::TcpConnection *conn_; // 绑定的connection
     CallbackMap cbMap_; // 回调函数映射容器
 	int error_;
     int id_;
@@ -68,7 +70,7 @@ class Multiplexer
 	char *location_ = NULL;
 
   public:
-    Multiplexer(ndsl::net::Connection *conn,
+    Multiplexer(ndsl::net::TcpConnection *conn,
     const CallbackMap &CallBackMap)
     :conn_(conn), cbMap_(CallBackMap){}
 
@@ -85,13 +87,13 @@ class Multiplexer
     static void remove(int id);
 
     // 向上层提供发送消息接口
-    static void sendMessage(
+    void sendMessage(
         int id,
         int len,
         char *data);
-    
+
     // 分发消息给通信实体
-    static void dispatch();
+    static void dispatch(void *p);
 
   private:
 	// 回调，释放已使用的内存
@@ -99,6 +101,6 @@ class Multiplexer
 };
 
 } // namespace net
-} // namespace ndsl 
+} // namespace ndsl
 
 #endif // __NDSL_NET_MULTIPLEXER_H__
--- src/ndsl/net/Multiplexer.cc
+++ src/ndsl/net/Multiplexer.cc
@@ -1,6 +1,6 @@
-/** 
+/**
 * @file Multiplexer.cc
-* @brief 
+* @brief
 * 实现功能：
 *  1.绑定connection
 *  2.插入通信实体
@@ -12,126 +12,141 @@
 * @emial 429834658@qq.com
 **/
 #include<string.h>
-#include "ndsl/net/Mutiplexer.h"
-#include "ndsl/net/Eventloop.h"
+#include "ndsl/net/Multiplexer.h"
+#include "ndsl/net/EventLoop.h"
 
 namespace ndsl{
 namespace net{
 
 // 在map中插入<id,callback>
-static void Multiplexer::insert( void *pa )
+static void Multiplexer::insert(void *pa )
 {
-    struct para *p = reinterpret_cast<struct para *>(pa);
+    struct para *p = static_cast<struct para *>(pa);
+	Multiplexer *pthis = p->pthis;
 
-    MultiplexerCallbackMap::iterator iter=cbMap_.lower_bound(p->id); // 返回map中第一个不小于id的迭代器指针
-    if(iter == cbMap_.end() || iter->first != p->id){
-        cbMap_.insert(iter,make_pair(p->id,p->cb)); 
+    Multiplexer::CallbackMap::iterator iter=pthis->cbMap_.lower_bound(p->id); // 返回map中第一个不小于id的迭代器指针
+    if(iter == pthis->cbMap_.end() || iter->first != p->id){
+        pthis->cbMap_.insert(iter,make_pair(p->id,p->cb));
     }
 
-	if (p != NULL)
+	if (p != NULL) // 释放para
 	{
 		delete p;
 		p = NULL;
 	}
 
-	conn_->onRecv(msghead_, &rlen_, dispatch, NULL); // 开始接收消息
+	conn_->onRecv(pthis->msghead_, &(pthis->rlen_), pthis->dispatch, (void *)pthis); // 开始接收消息
 }
 
 // 在loop工作队列中加入insert任务
-void Multiplexer::addInsertWork(int id, MultiplexerCallback cb)
+void Multiplexer::addInsertWork(int id, Callback cb)
 {
-    para *p = new para; // 在insert()中释放
+    struct para *p = new para; // 在insert()中释放
     p->id = id;
     p->cb = cb;
+	p->pthis = this;
 
     work_struct *w1 = new work_struct; // 在eventloop中释放
     w1->doit = insert;
-    w1->para = (void *)p;
+    w1->para = static_cast<void *>(p);
     conn_.loop_.addWork(w1);
 }
 
 // 在map中删除<id,callback>对
-static void Multiplexer::remove( void *i )
+static void Multiplexer::remove( void *pa )
 {
-    int id = static_cast<int>(i);
-    cbMap_.erase(id);
+	struct para *p = static_cast<struct para *>(pa);
+    int id = p->id;
+    p->pthis->cbMap_.erase(id);
+
+	if (p != NULL) // 释放para
+	{
+		delete p;
+		p = NULL;
+	}
 }
 
 // 在loop工作队列中加入remove任务
 void Multiplexer::addRemoveWork(int id)
 {
+	struct para *p = new para; // 在remove()中释放
+    p->id = id;
+    p->cb = NULL;
+	p->pthis = this;
+
     work_struct *w2 = new work_struct; // 在eventloop中释放
-    w1->doit = remove;
-    w1->para = (void *)id;
+    w2->doit = remove;
+    w2->para = static_cast<void *>(p);
     conn_.loop_.addWork(w2);
 }
 
 // 回调，释放已使用的内存
 static void Multiplexer::freecb(void &p)
 {
-	if (p != NULL) 
+	if (p != NULL)
 	{
 		free(p);
 		p = NULL;
 	}
 }
 
-// 向上层提供发送消息接口 
-static void Multiplexer::sendMessage(int id, int len,char *data)
+// 向上层提供发送消息接口
+void Multiplexer::sendMessage(int id, int len,char *data)
 {
-	buffer = (char *)malloc(sizeof(char)*len);  // TODO:释放内存
+	buffer = (char *)malloc(sizeof(char)*len);  // 在tcpconnection中释放
 	Message *message = reinterpret_cast<struct Message *>(buffer);
     message->id = id;
     message->len = len; // 消息头中的len为负载长度
 	strcpy(buffer + sizeof(Message), data, len);
 
-	conn_->onSend(buffer, len+sizeof(Message), -1, freecb, NULL);
+	conn_->onSend(buffer, len+sizeof(Message), -1, NULL, NULL);
 }
 
 // 分发消息给上层通信实体
-void Multiplexer::dispatch()
+static void Multiplexer::dispatch(void *p)
 {
-    if(left_ == 0) // 是新任务，处理读取消息头的逻辑
+	Multiplexer *pthis  = static_cast<Multiplexer *>(p);
+    if(pthis->left_ == 0) // 是新任务，处理读取消息头的逻辑
     {
-        struct Message *message = reinterpret_cast<struct Message *>(msghead_);
-        id_ = message->id;
-        len_ = message->len;
-		left_ = len_;
-		location_ = msghead_;
-	    rlen_ -=sizeof(int) * 2 // 对rlen_做更新，表示读到的数据长度
-		left_ -= rlen_; // 对left_做更新
-		location_ += sizeof(int)*2; // 定位到负载
+        struct Message *message = reinterpret_cast<struct Message *>(pthis->msghead_);
+        pthis->id_ = message->id;
+        pthis->len_ = message->len;
+		pthis->left_ = pthis->len_;
+		pthis->location_ = pthis->msghead_;
+	    pthis->rlen_ -=sizeof(int) * 2 // 对rlen_做更新，表示读到的数据长度
+		pthis->left_ -= pthis->rlen_; // 对left_做更新
+		pthis->location_ += sizeof(int)*2; // 定位到负载
 
-		if (left_== 0) // 已读完消息
+		if (pthis->left_== 0) // 已读完消息
 		{
-			MultiplexerCallbackMap::iterator iter = cbMap_.find(id_);
-			if (iter != cbMap_.end()) iter->second(location_, rlen_, error_); // 在这里调用了实体对应的回调函数
-			
-			conn_->onRecv(msghead_, &rlen_, dispatch, NULL); // 又开始新一轮读取
+			Multiplexer::CallbackMap::iterator iter = pthis->cbMap_.find(pthis->id_);
+			if (iter != pthis->cbMap_.end()) iter->second(pthis->location_, pthis->rlen_, pthis->error_); // 在这里调用了实体对应的回调函数
+
+			pthis->conn_->onRecv(pthis->msghead_, &(pthis->rlen_), pthis->dispatch, (void *)pthis); // 又开始新一轮读取
 		}
-		else if (left_ > 0)
+		else if (pthis->left_ > 0)
 		{
-            databuf_ = (char *)malloc(sizeof(char) * len_);  // TODO:释放内存
-            memcpy( databuf_, location_, rlen_); // 做一次拷贝开销不大
+            databuf_ = (char *)malloc(sizeof(char) * pthis->len_);  // TODO:释放内存
+            memcpy( pthis->databuf_, pthis->location_, pthis->rlen_); // 做一次拷贝开销不大
 		    // 原型void *memcpy(void*dest, const void *src, size_t n)
-			location_ = databuf_;
-			location_ += rlen_; // location指针向后滑动
-			conn_->onRecv(location_, &rlen_, dispatch, NULL);
+			pthis->location_ = pthis->databuf_;
+			pthis->location_ += pthis->rlen_; // location指针向后滑动
+			pthis->conn_->onRecv(pthis->location_, &(pthis->rlen_), pthis->dispatch, (void *)pthis);
 		}
     }
-    else if(left_ > 0) // 有剩余字节数未读
+    else if(pthis->left_ > 0) // 有剩余字节数未读
     {
-		left_ -= rlen_;
-		location_ += rlen_; // location指针向后滑动
-		if (left_== 0) // 已读完消息
+		pthis->left_ -= pthis->rlen_;
+		pthis->location_ += pthis->rlen_; // location指针向后滑动
+		if (pthis->left_== 0) // 已读完消息
 		{
-			MultiplexerCallbackMap::iterator iter = cbMap_.find(id_);
-			if (iter != cbMap_.end()) iter->second(databuf_, len_, error_);
-			
-			conn_->onRecv(msghead_, &rlen_, dispatch, NULL); // 又开始新一轮读取
+			Multiplexer::CallbackMap::iterator iter = pthis->cbMap_.find(pthis->id_);
+			if (iter != pthis->cbMap_.end()) iter->second(pthis->databuf_, pthis->len_, pthis->error_);
+
+			pthis->conn_->onRecv(pthis->msghead_, &(pthis->rlen_), pthis->dispatch, (void *)pthis); // 又开始新一轮读取
 		}
-		else if(left_ > 0) // 继续读取数据
-			conn_->onRecv(location_, &rlen_, dispatch, NULL);
+		else if(pthis->left_ > 0) // 继续读取数据
+			pthis->conn_->onRecv(pthis->location_, &(pthis->rlen_), pthis->dispatch, (void *)pthis);
     }
 }
 
