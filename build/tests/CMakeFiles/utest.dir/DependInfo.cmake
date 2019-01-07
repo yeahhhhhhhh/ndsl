787523293afc4b6bebef97ca7b3f040bc8629eca
myname <myname@gmail.com>
2019-01-07 18:47:49 +0800

    dev_why

---
+++ build/tests/CMakeFiles/utest.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/Desktop/ndsl/tests/testMain.cc" "/home/user/Desktop/ndsl/build/tests/CMakeFiles/utest.dir/testMain.cc.o"
+  "/home/user/Desktop/ndsl/tests/utils/LogTest.cc" "/home/user/Desktop/ndsl/build/tests/CMakeFiles/utest.dir/utils/LogTest.cc.o"
+  "/home/user/Desktop/ndsl/tests/utils/PluginTest.cc" "/home/user/Desktop/ndsl/build/tests/CMakeFiles/utest.dir/utils/PluginTest.cc.o"
+  "/home/user/Desktop/ndsl/tests/utils/TimeStampTest.cc" "/home/user/Desktop/ndsl/build/tests/CMakeFiles/utest.dir/utils/TimeStampTest.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/user/Desktop/ndsl/build/src/ndsl/CMakeFiles/ndsl.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/tests/CMakeFiles/utest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/utest.dir/testMain.cc.o"
+  "CMakeFiles/utest.dir/utils/LogTest.cc.o"
+  "CMakeFiles/utest.dir/utils/TimeStampTest.cc.o"
+  "CMakeFiles/utest.dir/utils/PluginTest.cc.o"
+  "../bin/utest.pdb"
+  "../bin/utest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/utest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
