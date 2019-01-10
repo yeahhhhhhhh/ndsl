384a5ea33081ce380f9c1ec30b83163f30d28b9f
asf <>
2019-01-10 16:24:59 +0800

    checkout to master

---
+++ tests/Plugintest/plugintest.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <dlfcn.h>
+#include <stdio.h>
+#include "ndsl/utils/Plugin.h"
+using namespace std;
+
+typedef Plugin *(*Creat)(int);
+void func(void *para) { std::cout << "hello world!\n " << std::endl; }
+
+int main(int argc, char **argv)
+{
+    void *p = NULL;
+    Plugin1 *plugin = NULL;
+    void *para = NULL;
+    p = dlopen(
+        "/home/zzt/winuxshare/ndsl/build/bin/libplugin.so",
+        RTLD_NOW | RTLD_DEEPBIND);
+    if (!p) {
+        cout << "cannot open library: " << dlerror() << endl;
+        return -1;
+    }
+    Creat fun = (Creat) dlsym(p, "CreatPlugin");
+    if (!fun) {
+        cout << "cannot load sysbol:  " << dlerror() << endl;
+        dlclose(p);
+        return -1;
+    }
+    plugin = (Plugin1 *) fun(1);
+    if (!plugin) { printf("plugin == NULL !\n"); }
+    plugin->doit(func, para);
+    return 0;
+}
