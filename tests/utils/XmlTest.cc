feb684c52b5539dc2159d518cc1646866ad3f5c0
zjc <2364464666@qq.com>
2019-01-15 15:00:18 +0800

    xml测试流找不到文件

--- tests/utils/XmlTest.cc
+++ tests/utils/XmlTest.cc
@@ -6,34 +6,36 @@
 // @author zjc
 // @email 2324234234@qq.com
 //
-
-//#define CATCH_CONFIG_MAIN
+#include "../catch.hpp"
+#include "ndsl/utils/Xml.h"
 #include <fstream>
-
-#include "test.h"
-#include "ndsl\include\ndsl\utils\Xml.h"
 
 TEST_CASE("Xml")
 {
-    std::ifstream stream("phone.xml");
-    Xml xml;
+    std::ifstream stream1("phone.xml");
+    for (std::string str; getline(stream1, str);) // getline()逐行读取文本
+        std::cout << str << std::endl;
+    // stream1->std::cout;
+    ndsl::utils::XML xml;
     xml_node node(NULL);
     xpath_node_set set;
     std::string str;
+    xml.loadstream(stream1);
     SECTION("xmlparse_test")
     {
-        REQUIRE(xml.loadstream(stream) == 0);
+        // REQUIRE(xml.loadstream(stream) == 0);
 
         REQUIRE(xml.getfirstnode("phone_books", node) == 0);
+        // node.print(std::cout);
 
         REQUIRE(xml.getnodesets("phone_books", set) == 0);
 
         //  xml.getfirstnode("Phone_books/phone", node);
 
-        REQUIRE(xml.getpeoperty(node, "id", str) == 0);
+        REQUIRE(xml.getproperty(node, "id", str) == 0);
         REQUIRE(xml.delproperty(node, "id") == 0);
-        REQUIRE(xml.delchidnode(node, "tel") == 0);
-        REQUIRE(xml.addChildNode(node, "shit") == 0);
+        REQUIRE(xml.delchildnode(node, "tel") == 0);
+        REQUIRE(xml.addchildnode(node, "shit") == 0);
         REQUIRE(xml.setnodetextandattr(node, "friend", "hap", "ship") == 0);
         REQUIRE(xml.savestream(std::cout) == 0);
         // REQUIRE()
