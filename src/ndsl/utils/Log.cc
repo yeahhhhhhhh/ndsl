87f6ffae853e013d6af038261b08f04491f0256b
BBlackB <675040625@qq.com>
2019-01-16 16:59:13 +0800

    调整文件

--- src/ndsl/utils/Log.cc
+++ src/ndsl/utils/Log.cc
@@ -17,7 +17,6 @@
 #include <ndsl/utils/Log.h>
 #include <ndsl/utils/TimeStamp.h>
 
-
 ////
 // @biref
 // 文件
@@ -67,7 +66,6 @@ static int log_source_tag = 35;
 static int tag = -1;
 static uint64_t _sinks = 0;
 
-
 void set_ndsl_log_sinks(uint64_t sinks, int file_or_ter) // file = 1, ter = 0
 {
     _sinks = sinks;
@@ -81,13 +79,19 @@ void set_ndsl_log_sinks(uint64_t sinks, int file_or_ter) // file = 1, ter = 0
 
 uint64_t add_source()
 {
-    uint64_t module = 1UL << log_source_tag ;
-    //printf("module = %lu\n",module);
-    log_source_tag ++;
+    uint64_t module = 1UL << log_source_tag;
+    // printf("module = %lu\n",module);
+    log_source_tag++;
     return module;
 }
 
-void ndsl_log_into_sink(int level, uint64_t source,const char* file_name,const char * func_name, const char *format, ...)
+void ndsl_log_into_sink(
+    int level,
+    uint64_t source,
+    const char *file_name,
+    const char *func_name,
+    const char *format,
+    ...)
 {
     ndsl::utils::TimeStamp ts;
     char buffer[4096] = {0};
@@ -96,11 +100,10 @@ void ndsl_log_into_sink(int level, uint64_t source,const char* file_name,const c
     buffer[0] = '[';
     ts.to_string(buffer + 1, 4096);
     int ret1 = ::strlen(buffer);
-    
 
     buffer[ret1] = ']';
 
-    const char * ptr = strrchr(file_name, '/');
+    const char *ptr = strrchr(file_name, '/');
 
     int ret2 = sprintf(
         buffer + ret1 + 1,
@@ -108,24 +111,20 @@ void ndsl_log_into_sink(int level, uint64_t source,const char* file_name,const c
         level,
         ::getpid(),
         (long) ::pthread_self(),
-        //file_name,
+        // file_name,
         ptr + 1,
-        func_name
-        ); // 毫秒
+        func_name); // 毫秒
 
-    
     // 复制数据
     va_list ap;
     va_start(ap, format);
     int ret3 =
         vsnprintf(buffer + ret1 + ret2 + 1, 512 - ret1 - ret2 - 1, format, ap);
     if (ret3 < 0) return;
- 
-    if((source & _sinks)&& (tag == 0))
-        printf("%s",buffer);
 
-    if ((source & _sinks) && (tag == 1)) { 
+    if ((source & _sinks) && (tag == 0)) printf("%s\n", buffer);
+
+    if ((source & _sinks) && (tag == 1)) {
         file_log.log(buffer, ret1 + ret2 + ret3 + 1);
     }
-    
 }
