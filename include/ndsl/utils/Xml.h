da3328828e5f3afb62e2a026a9db016404bb03f8
zzt <>
2019-01-23 22:14:34 +0800

    merge master

--- include/ndsl/utils/Xml.h
+++ include/ndsl/utils/Xml.h
@@ -8,10 +8,11 @@
 //
 #ifndef __NDSL_UTILS_XML_H__
 #define __NDSL_UTILS_XML_H__
-#include <libxml2/libxml/parser.h>
-#include <libxml2/libxml/tree.h>
-#include <libxml2/libxml/xpath.h>
-#include <libxml2/libxml/xmlreader.h>
+#include <libxml/parser.h>
+
+#include <libxml/tree.h>
+#include <libxml/xpath.h>
+#include <libxml/xmlreader.h>
 #include <string>
 #include <vector>
 
@@ -24,6 +25,7 @@ class Xml
   public:
     Xml();
     ~Xml();
+
     // 从内存中导入xml数据
     xmlNode *loadmemory(const char *xbuf, unsigned int size);
     //
@@ -36,6 +38,7 @@ class Xml
     //  获取子节点
     int
     getchildren(const xmlNode *curnode, std::vector<xmlNode *> &children) const;
+    int getvalue(xmlNode *node, std::string &value);
     // 从当前节点中获取某个子节点
     // int getspecialchild(const xmlNode *curnode, const char *nodename) const;
     // 从当前节点获取子节点集
@@ -46,7 +49,7 @@ class Xml
 
     // 获取节点属性
     int
-    getproperty(const xmlNode *node, const char *property, std::string &value);
+    getproperty(xmlNode *node, const char *property, std::string &value) const;
     // 设置节点属性
     int setproperty(xmlNode *node, const char *property, const char *value);
     // 删除属性
@@ -56,17 +59,19 @@ class Xml
     addsimplechild(xmlNode *parent, const char *nodename, const char *value);
     xmlNode *addchild(xmlNode *parent, xmlNode *cur);
 
-    int getfirstnodevalue(const char *xpath, std::string &value);
-
     xmlNode *getrootnode();
+    //将数据存入buffer
+    char *getxmlbuffer(); 
+    //将节点数据导入buffer
+    char* getnodebuffer(xmlNode* node );
+    void savefile();
 
   private:
     xmlDocPtr m_pdoc;          // doc
     xmlNodePtr m_proot;        // 根节点
     xmlBufferPtr m_pbuf;       // buffer
-    xmlTextReaderPtr m_reader; //
-    std::vector<xmlchar *> m_attrs;
-}；
+   
+};
 } // namespace utils
 } // namespace ndsl
 
